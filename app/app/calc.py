5010e2dc55e76392cccc88b69a757dfd82670417
Jean Paul Bernhardt <beralb@protonmail.com>
2023-03-27 15:37:04 -0300

    modifications in compose and dockerfile

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
+
 def add(x, y):
-    """ Adds x and y then returns result """
+    """Adds x and y then returns result"""
     return x + y
+
 
 def subtract(x, y):
     """Subtract y from x then returns results"""
