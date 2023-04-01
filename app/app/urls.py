eca4afd2df2796f3811ee708a5819170111bd831
Jean Paul Bernhardt <beralb@protonmail.com>
2023-04-01 08:04:38 -0300

    fix: urls linting

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,11 +21,11 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema', SpectacularAPIView.as_view(), name='api-schema'),
+    path("admin/", admin.site.urls),
+    path("api/schema", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        'api/docs',
-        SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs',
-    )    
+        "api/docs",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
 ]
